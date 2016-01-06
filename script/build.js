7ce55ecfa5b152e9808dcedf6b454a6d92b446ee
xudafeng <xudafeng@126.com>
2016-01-06 15:55:30 +0800

    add build script

---
+++ script/build.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+/* ================================================================
+ * android-build by xdf(xudafeng[at]126.com)
+ *
+ * first created at : Wed Aug 26 2015 11:55:14 GMT+0800 (CST)
+ *
+ * ================================================================
+ * Copyright  xdf
+ *
+ * Licensed under the MIT License
+ * You may not use this file except in compliance with the License.
+ *
+ * ================================================================ */
+
+'use strict';
+
+var path = require('path');
+var antBuild = require('ant-build');
+
+antBuild({
+  cwd: path.join(__dirname, '..')
+}).then(function(d) {
+  console.log(d);
+});
